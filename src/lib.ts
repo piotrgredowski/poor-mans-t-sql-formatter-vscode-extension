ee32d06fa7109fe80038ae73aebf93f22dff1217
Piotr Grędowski <piotrgredowski@gmail.com>
2022-05-18 22:13:12 +0200

    Clean code and fix problem with format selection
    Probably fixes #4

--- src/lib.ts
+++ src/lib.ts
@@ -11,12 +11,7 @@ export interface ISqlFormatterResult {
 function getWholeDocRange(doc: vscode.TextDocument) {
   var firstLine = doc.lineAt(0);
   var lastLine = doc.lineAt(doc.lineCount - 1);
-  return new vscode.Range(
-    0,
-    firstLine.range.start.character,
-    doc.lineCount - 1,
-    lastLine.range.end.character,
-  );
+  return new vscode.Range(firstLine.range.start, lastLine.range.end);
 }
 
 function collectOptions(
@@ -64,11 +59,11 @@ export function formatDocument(editorFormattingOptions: vscode.FormattingOptions
   let editor = vscode.window.activeTextEditor as TextEditor;
   let document = editor.document;
   let useSelection = !editor.selection.isEmpty;
-  let selectionActivePoint = editor.selection.active;
   let inputSql = useSelection ? document.getText(editor.selection) : document.getText();
 
-  let oldOffset = document.offsetAt(selectionActivePoint);
-  let oldOffsetPercent = inputSql.length === 0 ? 0 : oldOffset / inputSql.length;
+  const selectionRange = useSelection
+    ? new vscode.Range(editor.selection.start, editor.selection.end)
+    : getWholeDocRange(document);
 
   let extensionSettingsObject = vscode.workspace.getConfiguration('poor-mans-t-sql-formatter-pg');
   let options = collectOptions(extensionSettingsObject, editorFormattingOptions);
@@ -102,50 +97,9 @@ export function formatDocument(editorFormattingOptions: vscode.FormattingOptions
     }
 
     const result = sqlFormatter.formatSql(inputSql, options) as ISqlFormatterResult;
-    const formatDespiteError = new Promise((resolve1, reject1) => {
-      if (extensionSettingsObject.confirmOnError && result.errorFound) {
-        vscode.window
-          .showWarningMessage(
-            'The content you are attempting to format was not successfully parsed, and the formatted result may not match the original intent. Would you like to format anyway?',
-            'Format Anyway',
-          )
-          .then((selectedAction1) => {
-            if (selectedAction1 === 'Format Anyway') {
-              resolve1(true);
-            } else {
-              resolve1(false);
-            }
-          });
-      } else {
-        resolve1(true);
-      }
-    });
-    return result.text;
-    // const choice = Promise.resolve(formatDespiteError);
-    // const final = choice.then(function (c) {
-    //   if (c) {
-    //     return result.text;
-    //     // editor
-    //     //   .edit((mutator) => {
-    //     //     mutator.replace(
-    //     //       useSelection ? editor.selection : getWholeDocRange(document),
-    //     //       result.text,
-    //     //     );
-    //     //   })
-    //     //   .then((editCompleted) => {
-    //     //     if (!useSelection) {
-    //     //       //put the cursor back in approximately the same (relative) place we found it...
-    //     //       let newOffset = oldOffsetPercent * result.text.length;
-    //     //       let newPosition = document.positionAt(newOffset);
-    //     //       editor.selection = new vscode.Selection(newPosition, newPosition);
-    //     //     }
-    //     //   });
-    //   } else {
-    //     return '';
-    //   }
-    //   //   return result;
-    // });
-    // return final;
+
+    return [vscode.TextEdit.replace(selectionRange, result.text)];
+
   } catch (e) {
     console.log(e);
     throw e;
